0f7a85d01f236eea042d3a5e1d043ba811621703
LiemDev <buithanhliem5073@gmai.com>
2025-04-04 09:22:41 +0700

    initial router

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,23 @@
 import express from 'express'
+
 const app = express()
 const port = 3000
+const router = express.Router()
+
+app.use((req, res, next) => {
+  console.log(new Date())
+  next()
+})
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
+
+router.get('/tweets', (req, res) => {
+  res.json({ message: 'data tweets' })
+})
+
+app.use('/api', router)
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
